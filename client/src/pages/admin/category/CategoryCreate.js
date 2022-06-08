6134fe7ffeefd6b8b5e0f08350ec02753f15cd5f
onurtaskirancom <taskiranonur9@gmail.com>
2022-06-09 02:12:04 +0300

    Code Refactor - Reusable Component

--- client/src/pages/admin/category/CategoryCreate.js
+++ client/src/pages/admin/category/CategoryCreate.js
@@ -9,6 +9,7 @@ import {
 } from "../../../functions/category";
 import { Link } from "react-router-dom";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import CategoryForm from "../../../components/forms/CategoryForm";
 
 const CategoryCreate = () => {
   const { user } = useSelector((state) => ({ ...state }));
@@ -63,50 +64,38 @@ const CategoryCreate = () => {
     }
   };
 
-  const categoryForm = () => (
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label>Name</label>
-        <input
-          type="text"
-          className="form-control"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-          autoFocus
-          required
-        />
-        <br />
-        <button className="btn btn-outline-primary">Save</button>
-      </div>
-    </form>
-  );
-
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-2">
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-md-2'>
           <AdminNav />
         </div>
-        <div className="col">
+        <div className='col'>
           {loading ? (
-            <h4 className="text-danger">Loading..</h4>
+            <h4 className='text-danger'>Loading..</h4>
           ) : (
             <h4>Create category</h4>
           )}
-          {categoryForm()}
+
+          <CategoryForm
+            handleSubmit={handleSubmit}
+            name={name}
+            setName={setName}
+          />
+
           <hr />
           {categories.map((c) => (
-            <div className="alert alert-secondary" key={c._id}>
+            <div className='alert alert-secondary' key={c._id}>
               {c.name}
               <span
                 onClick={() => handleRemove(c.slug)}
-                className="btn btn-sm float-right"
+                className='btn btn-sm float-right'
               >
-                <DeleteOutlined className="text-danger" />
+                <DeleteOutlined className='text-danger' />
               </span>
               <Link to={`/admin/category/${c.slug}`}>
-                <span className="btn btn-sm float-right">
-                  <EditOutlined className="text-warning" />
+                <span className='btn btn-sm float-right'>
+                  <EditOutlined className='text-warning' />
                 </span>
               </Link>
             </div>
