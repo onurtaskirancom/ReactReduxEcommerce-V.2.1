5f328e87007a1c88f32743c52abca85d53e46267
onurtaskirancom <taskiranonur9@gmail.com>
2022-06-22 07:32:18 +0300

    Create Product

--- client/src/pages/admin/product/ProductCreate.js
+++ client/src/pages/admin/product/ProductCreate.js
@@ -23,6 +23,9 @@ const initialState = {
 const ProductCreate = () => {
   const [values, setValues] = useState(initialState);
 
+  // redux
+  const { user } = useSelector((state) => ({ ...state }));
+
   // destructure
   const {
     title,
@@ -42,87 +45,95 @@ const ProductCreate = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    //
+    createProduct(values, user.token)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+        if (err.response.status === 400) toast.error(err.response.data);
+      });
   };
 
   const handleChange = (e) => {
-    //
+    setValues({ ...values, [e.target.name]: e.target.value });
+    // console.log(e.target.name, " ----- ", e.target.value);
   };
 
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-2">
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-md-2'>
           <AdminNav />
         </div>
 
-        <div className="col-md-10">
+        <div className='col-md-10'>
           <h4>Product create</h4>
           <hr />
 
           <form onSubmit={handleSubmit}>
-            <div className="form-group">
+            <div className='form-group'>
               <label>Title</label>
               <input
-                type="text"
-                name="title"
-                className="form-control"
+                type='text'
+                name='title'
+                className='form-control'
                 value={title}
                 onChange={handleChange}
               />
             </div>
 
-            <div className="form-group">
+            <div className='form-group'>
               <label>Description</label>
               <input
-                type="text"
-                name="description"
-                className="form-control"
+                type='text'
+                name='description'
+                className='form-control'
                 value={description}
                 onChange={handleChange}
               />
             </div>
 
-            <div className="form-group">
+            <div className='form-group'>
               <label>Price</label>
               <input
-                type="number"
-                name="price"
-                className="form-control"
+                type='number'
+                name='price'
+                className='form-control'
                 value={price}
                 onChange={handleChange}
               />
             </div>
 
-            <div className="form-group">
+            <div className='form-group'>
               <label>Shipping</label>
               <select
-                name="shipping"
-                className="form-control"
+                name='shipping'
+                className='form-control'
                 onChange={handleChange}
               >
                 <option>Please select</option>
-                <option value="No">No</option>
-                <option value="Yes">Yes</option>
+                <option value='No'>No</option>
+                <option value='Yes'>Yes</option>
               </select>
             </div>
 
-            <div className="form-group">
+            <div className='form-group'>
               <label>Quantity</label>
               <input
-                type="number"
-                name="quantity"
-                className="form-control"
+                type='number'
+                name='quantity'
+                className='form-control'
                 value={quantity}
                 onChange={handleChange}
               />
             </div>
 
-            <div className="form-group">
+            <div className='form-group'>
               <label>Color</label>
               <select
-                name="color"
-                className="form-control"
+                name='color'
+                className='form-control'
                 onChange={handleChange}
               >
                 <option>Please select</option>
@@ -134,11 +145,11 @@ const ProductCreate = () => {
               </select>
             </div>
 
-            <div className="form-group">
+            <div className='form-group'>
               <label>Brand</label>
               <select
-                name="brand"
-                className="form-control"
+                name='brand'
+                className='form-control'
                 onChange={handleChange}
               >
                 <option>Please select</option>
@@ -150,7 +161,7 @@ const ProductCreate = () => {
               </select>
             </div>
 
-            <button className="btn btn-outline-info">Save</button>
+            <button className='btn btn-outline-info'>Save</button>
           </form>
         </div>
       </div>
