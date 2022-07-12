88ac14d37a04657da31b93f277bb04cfdad0ed53
onurtaskirancom <taskiranonur9@gmail.com>
2022-07-12 13:33:19 +0300

    Admin Products Card

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import AdminNav from "../../components/nav/AdminNav";
 import { getProductsByCount } from "../../functions/product";
+import AdminProductCard from "../../components/cards/AdminProductCard";
 
 const AdminDashboard = () => {
   const [products, setProducts] = useState([]);
@@ -24,17 +25,26 @@ const AdminDashboard = () => {
   };
 
   return (
-    <div className='container-fluid'>
-      <div className='row'>
-        <div className='col-md-2'>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
           <AdminNav />
         </div>
-        {loading ? (
-          <h4 className='text-danger'>Loading...</h4>
-        ) : (
-          <h4>All Products</h4>
-        )}
-        <div className='col'>{JSON.stringify(products)}</div>
+
+        <div className="col">
+          {loading ? (
+            <h4 className="text-danger">Loading...</h4>
+          ) : (
+            <h4>All Products</h4>
+          )}
+          <div className="row">
+            {products.map((product) => (
+              <div key={product._id} className="col-md-4">
+                <AdminProductCard product={product} />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
