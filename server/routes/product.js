bca54b1e4c09a84ef15e14cfeeecf17c54a02474
onurtaskirancom <taskiranonur9@gmail.com>
2022-08-02 18:16:22 +0300

    List Product with Sort Order and Limit

--- server/routes/product.js
+++ server/routes/product.js
@@ -11,13 +11,16 @@ const {
   remove,
   read,
   update,
+  list,
 } = require("../controllers/product");
 
 // routes
 router.post("/product", authCheck, adminCheck, create);
-router.get("/products/:count", listAll); //products/100
+router.get("/products/:count", listAll); // products/100
 router.delete("/product/:slug", authCheck, adminCheck, remove);
 router.get("/product/:slug", read);
 router.put("/product/:slug", authCheck, adminCheck, update);
+
+router.post("/products", list);
 
 module.exports = router;
