0af017be879bc35512d5e3a5ee403fd7d40cfc50
onurtaskirancom <taskiranonur9@gmail.com>
2022-08-03 13:54:17 +0300

    Added New Arrivals

--- client/src/functions/product.js
+++ client/src/functions/product.js
@@ -17,12 +17,19 @@ export const removeProduct = async (slug, authtoken) =>
     },
   });
 
-  export const getProduct = async (slug) =>
+export const getProduct = async (slug) =>
   await axios.get(`${process.env.REACT_APP_API}/product/${slug}`);
 
-  export const updateProduct = async (slug, product, authtoken) =>
+export const updateProduct = async (slug, product, authtoken) =>
   await axios.put(`${process.env.REACT_APP_API}/product/${slug}`, product, {
     headers: {
       authtoken,
     },
+  });
+
+export const getProducts = async (sort, order, limit) =>
+  await axios.post(`${process.env.REACT_APP_API}/products`, {
+    sort,
+    order,
+    limit,
   });
